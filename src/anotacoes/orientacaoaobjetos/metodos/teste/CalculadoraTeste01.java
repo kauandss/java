b854d51e40b0d5327e21bfa8ddc1b5a195da8fc8
Kauan Dias <102330011+kauandss@users.noreply.github.com>
2026-09-04 22:13:39 -0300

    add + anotações
    Comecei o tópico de métodos.

---
+++ src/anotacoes/orientacaoaobjetos/metodos/teste/CalculadoraTeste01.java
@@ -0,0 +1,11 @@
+package anotacoes.orientacaoaobjetos.metodos.teste;
+
+import anotacoes.orientacaoaobjetos.metodos.dominio.Calculadora;
+
+public class CalculadoraTeste01 {
+    static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+        calculadora.subtraiDoisNumeros();
+    }
+}
---
+++ src/anotacoes/orientacaoaobjetos/metodos/teste/CalculadoraTeste02.java
@@ -0,0 +1,10 @@
+package anotacoes.orientacaoaobjetos.metodos.teste;
+
+import anotacoes.orientacaoaobjetos.metodos.dominio.Calculadora;
+
+public class CalculadoraTeste02 {
+    static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicaDoisNumeros(20, 10);
+    }
+}
