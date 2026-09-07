2574cff05782896df935b07e1292563e1a0ccc3d
Kauan Dias <102330011+kauandss@users.noreply.github.com>
2026-09-07 14:33:21 -0300

    add indentação

--- src/anotacoes/orientacaoaobjetos/metodos/teste/CalculadoraTeste03.java
+++ src/anotacoes/orientacaoaobjetos/metodos/teste/CalculadoraTeste03.java
@@ -11,13 +11,13 @@ public class CalculadoraTeste03 {
              Como 'divideDoisNumeros' retorna um double, podemos armazenar
              o resultado em uma variável para usar depois (resultado).
          */
-        double resultado = calculadora.divideDoisNumeros(20,0);
+        double resultado = calculadora.divideDoisNumeros(20, 0);
         System.out.println(resultado);
 
         /*
                 2. Chamada de mét0do void que faz a validação interna:
              Não retorna nada para guardar em variável; ele mesmo decide o que imprimir
          */
-        calculadora.imprimideDivisaoDeDoisNumeros(86,0);
+        calculadora.imprimideDivisaoDeDoisNumeros(86, 0);
     }
 }
