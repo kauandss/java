8cb47eb678cd2476406e2b3fa7f225cc6821c2ad
Kauan Dias <102330011+kauandss@users.noreply.github.com>
2026-09-07 19:20:10 -0300

    add + comentários

--- src/anotacoes/orientacaoaobjetos/metodos/dominio/ImpressoraEstudante.java
+++ src/anotacoes/orientacaoaobjetos/metodos/dominio/ImpressoraEstudante.java
@@ -2,12 +2,17 @@ package anotacoes.orientacaoaobjetos.metodos.dominio;
 
 public class ImpressoraEstudante {
     public void imprime(Estudante estudante){
+        // 'estudante' recebeu o endereço do objeto original criado no main.
         System.out.println("\n-------------------------------\n");
 
         System.out.println(estudante.nome);
         System.out.println(estudante.idade);
         System.out.println(estudante.sexo);
 
+        /*
+            ATENÇÃO: Aqui você usou a referência para entrar no objeto original e mudar seu campo!
+           Qualquer um que aponte para este mesmo objeto verá o nome "Letícia".
+         */
         estudante.nome = "Letícia";
     }
 }
