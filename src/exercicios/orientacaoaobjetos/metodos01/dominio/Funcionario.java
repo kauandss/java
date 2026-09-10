98cc3cf52b48755d06568e041df581bd0713b732
Kauan Dias <102330011+kauandss@users.noreply.github.com>
2026-09-10 12:00:11 -0300

    alt exercício (Funcionários)
    adicionei tudo aql que eu aprendi de modificador de acesso (private, set e get)

--- src/exercicios/orientacaoaobjetos/metodos01/dominio/Funcionario.java
+++ src/exercicios/orientacaoaobjetos/metodos01/dominio/Funcionario.java
@@ -12,9 +12,10 @@ package exercicios.orientacaoaobjetos.metodos01.dominio;
  * <p>2. Para tirar a média dos salários e imprimir o resultado.</p>
  */
 public class Funcionario {
-    public String nome;
-    public int idade;
-    public double[] salarios;
+    private String nome;
+    private int idade;
+    private double[] salarios;
+    private double media;
 
     public void imprime() {
 
@@ -36,12 +37,38 @@ public class Funcionario {
             return;
         }
 
-        double media = 0;
-
         for (double salario : salarios) {
             media += salario;
         }
         media /= salarios.length;
         System.out.println("\nMédia salarial: " + media);
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
+    }
+
+    public double getMedia() {
+        return media;
     }
 }
--- src/exercicios/orientacaoaobjetos/metodos01/teste/FuncionarioTeste.java
+++ src/exercicios/orientacaoaobjetos/metodos01/teste/FuncionarioTeste.java
@@ -5,11 +5,12 @@ import exercicios.orientacaoaobjetos.metodos01.dominio.Funcionario;
 public class FuncionarioTeste {
     static void main(String[] args) {
         Funcionario funcionario = new Funcionario();
-        funcionario.nome = "Kauan";
-        funcionario.idade = 19;
-        funcionario.salarios = new double[]{2500, 3000.50, 2760};
+        funcionario.setNome("Kauan");
+        funcionario.setIdade(19);
+        funcionario.setSalarios(new double[]{2500, 3000.50, 2760});
 
         funcionario.imprime();
+        System.out.println("Média: "+ funcionario.getMedia());
 
     }
 }
