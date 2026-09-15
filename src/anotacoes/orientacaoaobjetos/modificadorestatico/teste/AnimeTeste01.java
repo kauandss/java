e6ad41bc42f2c9423d3fbdcef9159e084db221a5
Kauan Dias <102330011+kauandss@users.noreply.github.com>
2026-09-15 11:25:47 -0300

    add anotações
    finalizei a aula de blocos de inicialização estáticos

---
+++ src/anotacoes/orientacaoaobjetos/modificadorestatico/teste/AnimeTeste01.java
@@ -0,0 +1,13 @@
+package anotacoes.orientacaoaobjetos.modificadorestatico.teste;
+
+import anotacoes.orientacaoaobjetos.modificadorestatico.dominio.Anime;
+
+public class AnimeTeste01 {
+    static void main(String[] args) {
+        Anime anime1 = new Anime();
+        Anime anime2 = new Anime();
+        Anime anime3 = new Anime();
+
+
+    }
+}
