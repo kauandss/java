ffd600881397f0bbaa1c9e2f4afb683441c5014b
Kauan Dias <102330011+kauandss@users.noreply.github.com>
2026-09-17 16:14:21 -0300

    alt Professor + SeminarioTeste
    Fiz um pequeno ajuste para o funcionamento do programa.

--- src/exercicios/orientacaoaobjetos/associacao01/dominio/Professor.java
+++ src/exercicios/orientacaoaobjetos/associacao01/dominio/Professor.java
@@ -5,20 +5,39 @@ public class Professor {
     private String especialidade;
     private Seminario[] seminarios;
 
-    public Professor(String nome, String especialidade){
+    public Professor(String nome) {
+        this.nome = nome;
+    }
+
+    public Professor(String nome, String especialidade) {
         this.nome = nome;
         this.especialidade = especialidade;
     }
 
-    public void imprimir(){
-        System.out.println("Nome: "+ this.nome);
-        System.out.println("Especialidade: "+ this.especialidade);
+    public Professor(String nome, String especialidade, Seminario[] seminarios) {
+        this.nome = nome;
+        this.especialidade = especialidade;
+        this.seminarios = seminarios;
+    }
 
-        if(seminarios == null) return;
-        for(Seminario seminario : seminarios){
+    public void imprimir() {
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Especialidade: " + this.especialidade);
+
+        if (seminarios == null) return;
+        System.out.println("\n--- Seminários Cadastrados ---");
+        for (Seminario seminario : seminarios) {
             System.out.println("Seminário: " + seminario.getTitulo());
-            System.out.println("Local: "+ seminario.getLocal().getEndereco());
+            System.out.println("Local: " + seminario.getLocal().getEndereco());
+            if (seminario.getAlunos() == null) continue;
+            System.out.println("\n--- Alunos ---");
+            for (Aluno aluno : seminario.getAlunos()) {
+                System.out.println("Aluno: " + aluno.getNome() + " | Idade: " + aluno.getIdade());
+
+            }
+
         }
+
 
     }
 
--- src/exercicios/orientacaoaobjetos/associacao01/teste/SeminarioTeste.java
+++ src/exercicios/orientacaoaobjetos/associacao01/teste/SeminarioTeste.java
@@ -7,20 +7,19 @@ import exercicios.orientacaoaobjetos.associacao01.dominio.Seminario;
 
 public class SeminarioTeste {
     static void main(String[] args) {
+
         Local local = new Local("Av. Tiradentes, 615 - Bom Retiro, São Paulo");
+
         Professor professor = new Professor("William", "Programação");
+
         Aluno aluno1 = new Aluno("Kauan", 19);
         Aluno aluno2 = new Aluno("Vinicius", 20);
-        Seminario seminario1 = new Seminario("Associação", local);
+        Aluno[] alunosParaSeminario = new Aluno[]{aluno1, aluno2};
 
-        Aluno[] alunos = new Aluno[]{aluno1, aluno2};
-        Seminario[] seminarios = new Seminario[]{seminario1};
+        Seminario seminario = new Seminario("Associação", alunosParaSeminario, local);
+        Seminario[] seminariosDisponiveis = new Seminario[]{seminario};
 
-        seminario1.setAlunos(alunos);
-        professor.setSeminarios(seminarios);
-        aluno1.setSeminario(seminario1);
-        aluno2.setSeminario(seminario1);
-
+        professor.setSeminarios(seminariosDisponiveis);
         professor.imprimir();
     }
 }
