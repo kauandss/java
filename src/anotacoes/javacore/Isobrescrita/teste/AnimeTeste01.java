74152aac5e2ce5b6634a97252e78e8e7d912326b
Kauan Dias <102330011+kauandss@users.noreply.github.com>
2026-09-23 13:00:34 -0300

    add anotacoes
    Aula = Sobrescrita do método

---
+++ src/anotacoes/javacore/Isobrescrita/teste/AnimeTeste01.java
@@ -0,0 +1,10 @@
+package anotacoes.javacore.Isobrescrita.teste;
+
+import anotacoes.javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTeste01 {
+    static void main(String[] args) {
+        Anime anime = new Anime("Dr.Stone");
+        System.out.println(anime);
+    }
+}
