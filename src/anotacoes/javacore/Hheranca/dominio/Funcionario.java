170e3993b1b86a586daeffa2b1e6f83200f4bfeb
Kauan Dias <102330011+kauandss@users.noreply.github.com>
2026-09-23 11:44:23 -0300

    alt Funcionario.java and Pessoa.java
    Vi a aula de herança sobre "protected"

--- src/anotacoes/javacore/Hheranca/dominio/Funcionario.java
+++ src/anotacoes/javacore/Hheranca/dominio/Funcionario.java
@@ -1,11 +1,15 @@
 package anotacoes.javacore.Hheranca.dominio;
 
-public class Funcionario extends Pessoa{
+public class Funcionario extends Pessoa {
     private double salario;
 
-    public void imprime(){
+    public void imprime() {
         super.imprime();
         System.out.println(this.salario);
+    }
+
+    public void relatorioPagamento() {
+        System.out.println("Eu " + this.nome + " recebi o salário de " + this.salario);
     }
 
     public double getSalario() {
