58f0e1f1cd2e5fedb984f0f31eeff35f7e95e270
Kauan Dias <102330011+kauandss@users.noreply.github.com>
2026-09-23 11:26:11 -0300

    add anotações
    Assisti a primeira aula de Herança

---
+++ src/anotacoes/javacore/Hheranca/teste/HerancaTeste01.java
@@ -0,0 +1,29 @@
+package anotacoes.javacore.Hheranca.teste;
+
+import anotacoes.javacore.Hheranca.dominio.Endereco;
+import anotacoes.javacore.Hheranca.dominio.Funcionario;
+import anotacoes.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTeste01 {
+    static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("01050-035");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Kauan");
+        pessoa.setCpf("113.431.243-02");
+        pessoa.setEndereco(endereco);
+
+        pessoa.imprime();
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Sara");
+        funcionario.setCpf("254.615.321-04");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(20000);
+
+        System.out.println("------------");
+        funcionario.imprime();
+
+    }
+}
