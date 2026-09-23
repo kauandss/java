cfc6dc639dd9162f4744f3c08ee5a4a9632daba0
Kauan Dias <102330011+kauandss@users.noreply.github.com>
2026-09-23 12:51:58 -0300

    add anotacoes
    Assisti a ultima aula de herança sobre sequência de inicialização

---
+++ src/anotacoes/javacore/Hheranca/teste/HerancaTeste02.java
@@ -0,0 +1,20 @@
+package anotacoes.javacore.Hheranca.teste;
+
+import anotacoes.javacore.Hheranca.dominio.Funcionario;
+
+public class HerancaTeste02 {
+    // 0 - Bloco de inicialização estático da superclasse é executado quando a JVM carregar a classe pai;
+    // 1 - Bloco de inicialização estático da subclasse é executado quando a JVM carregar a classe filha;
+    // 2 - Alocado espaço em memória pro objeto da superclasse pai;
+    // 3 - Cada atributo de superclasse pai é criado e inicializado com valores default ou o que for passado;
+    // 4 - Bloco de inicialização da superclasse é executado na ordem em que aparece;
+    // 5 - Construtor da superclasse é executado;
+
+    // 6 - Alocado espaço em memória pro objeto da subclasse filha;
+    // 7 - Cada atributo de subclasse pai é criado e inicializado com valores default ou o que for passado;
+    // 8 - Bloco de inicialização da subclasse é executado na ordem em que aparece;
+    // 9 - Construtor da subclasse é executado.
+    static void main(String[] args) {
+        Funcionario funcionario = new Funcionario("Kauan");
+    }
+}
