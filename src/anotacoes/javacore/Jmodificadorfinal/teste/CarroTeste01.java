1fb7e9734a09a762eb5bf6951d78837e7bc95bf3
Kauan Dias <102330011+kauandss@users.noreply.github.com>
2026-09-24 17:19:41 -0300

    add anotações
    1- aula modificador static em tipos primitivos

---
+++ src/anotacoes/javacore/Jmodificadorfinal/teste/CarroTeste01.java
@@ -0,0 +1,10 @@
+package anotacoes.javacore.Jmodificadorfinal.teste;
+
+import anotacoes.javacore.Jmodificadorfinal.dominio.Carro;
+
+public class CarroTeste01 {
+    static void main(String[] args) {
+        Carro carro = new Carro();
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+    }
+}
