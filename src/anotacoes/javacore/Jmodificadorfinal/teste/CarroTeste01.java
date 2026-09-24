eb487da51d44680b1fd85657240713e773d0d1a8
Kauan Dias <102330011+kauandss@users.noreply.github.com>
2026-09-24 17:26:54 -0300

    add anotações
    2- aula do modificador final (tipo referência)

--- src/anotacoes/javacore/Jmodificadorfinal/teste/CarroTeste01.java
+++ src/anotacoes/javacore/Jmodificadorfinal/teste/CarroTeste01.java
@@ -1,10 +1,15 @@
 package anotacoes.javacore.Jmodificadorfinal.teste;
 
 import anotacoes.javacore.Jmodificadorfinal.dominio.Carro;
+import anotacoes.javacore.Jmodificadorfinal.dominio.Comprador;
 
 public class CarroTeste01 {
     static void main(String[] args) {
         Carro carro = new Carro();
+
         System.out.println(Carro.VELOCIDADE_LIMITE);
+        System.out.println(carro.COMPRADOR);
+        carro.COMPRADOR.setNome("Corolla");
+        System.out.println(carro.COMPRADOR);
     }
 }
